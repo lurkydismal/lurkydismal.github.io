4d73258a649da226fc83773477f7cf0f8792195f
lurkydismal <lurkydismal@duck.com>
2023-08-06 03:04:04 -0700

    Format files

--- js/get_repos.js
+++ js/get_repos.js
@@ -2,7 +2,8 @@ function readFile( _path, _callback = console.log ) {
     let l_xmlHttp = new XMLHttpRequest();
 
     l_xmlHttp.onreadystatechange = function() {
-        if ( ( l_xmlHttp.readyState == XMLHttpRequest.DONE ) && ( l_xmlHttp.status == 200 ) ) {
+        if ( ( l_xmlHttp.readyState == XMLHttpRequest.DONE ) &&
+             ( l_xmlHttp.status == 200 ) ) {
             _callback( l_xmlHttp.responseText );
         }
     };
@@ -12,8 +13,9 @@ function readFile( _path, _callback = console.log ) {
 }
 
 const g_repositoryOwnerName = "lurkydismal";
-const g_branchName          = "main";
-const g_profileLink         = `https://raw.githubusercontent.com/${ g_repositoryOwnerName }`;
+const g_branchName = "main";
+const g_profileLink =
+    `https://raw.githubusercontent.com/${g_repositoryOwnerName}`;
 
 var g_markdownRenderer = window.markdownit();
 
@@ -21,13 +23,12 @@ var g_markdownRenderer = window.markdownit();
     for ( let _repositoryDiv of document.querySelectorAll( ".repository" ) ) {
         let l_repositoryName = _repositoryDiv.dataset[ "repository" ];
 
-        readFile(
-            g_profileLink + "\\" + l_repositoryName + "\\" + g_branchName + "\\" + "README.md",
-            _callback = (
-                ( _mdContent ) => {
-                    _repositoryDiv.innerHTML = g_markdownRenderer.render( _mdContent.split("## Table of Contents").at(0) );
-                }
-            )
-        );
+        readFile( g_profileLink + "\\" + l_repositoryName + "\\" +
+                      g_branchName + "\\" +
+                      "README.md",
+                  _callback = ( ( _mdContent ) => {
+                      _repositoryDiv.innerHTML = g_markdownRenderer.render(
+                          _mdContent.split( "## Table of Contents" ).at( 0 ) );
+                  } ) );
     }
 } )();
